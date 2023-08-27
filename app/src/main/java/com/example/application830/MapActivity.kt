a6bd34fb278127b75d36d1f8d4fcc3749a2812ab
s00y0ung <112811331+s00y0ung@users.noreply.github.com>
2023-08-28 04:17:44 +0900

    map 현재 위치
    사용자 위치 표시 가능

--- app/src/main/java/com/example/application830/MapActivity.kt
+++ app/src/main/java/com/example/application830/MapActivity.kt
@@ -1,8 +1,11 @@
 package com.example.application830
 
+import androidx.lifecycle.Transformations.map
+
+
 import android.Manifest
 import android.annotation.SuppressLint
-import android.hardware.Camera
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
@@ -11,36 +14,41 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Transformations.map
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback{
 
-    private var mMap: GoogleMap? = null
+    private lateinit var map: GoogleMap
 
     lateinit var locationPermission : ActivityResultLauncher<Array<String>>
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient //위치 서비스가 gps 사용해서 위치를 확인
+    lateinit var fusedLocationClient: FusedLocationProviderClient //위치 서비스가 gps 사용해서 위치를 확인
     lateinit var locationCallback: LocationCallback //위치 값 요청에 대한 갱신 정보를 받는 변수
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        /*val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment!!.getMapAsync(this)
-*/
+
         locationPermission = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()){ results ->
             if(results.all{it.value}){
-                startProcess()
+                val mapFragment =
+                    supportFragmentManager.findFragmentById(R.id.mapView) as SupportMapFragment?
+                mapFragment?.getMapAsync(this)
             }else
             {
                 Toast.makeText(this,"승인 권한이 필요합니다", Toast.LENGTH_LONG).show()
@@ -54,59 +62,68 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 Manifest.permission.ACCESS_FINE_LOCATION
             )
         )
-
     }
 
-    fun startProcess(){
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
-
-    // NULL이 아닌 GoogleMap 객체를 파라미터로 제공해 줄 수 있을 때 호출
     override fun onMapReady(googleMap: GoogleMap) {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        map = googleMap
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         updateLocation()
-        /*mMap = googleMap
-        val SEOUL = LatLng(37.556, 126.97)
-        val markerOptions = MarkerOptions()
-        markerOptions.position(SEOUL)
-        markerOptions.title("서울")
-        markerOptions.snippet("한국 수도")
-        mMap!!.addMarker(markerOptions)
-        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10f)) */
     }
 
     @SuppressLint("MissingPermission")
-    fun updateLocation()
-    {
+    fun updateLocation(){
         val locationRequest = LocationRequest.create()
-        locationRequest.run{
+        locationRequest.run {
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
             interval = 1000
         }
+        var flag = 1
 
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(p0: LocationResult) {
-                p0?.let{
-                    for (location in it.locations){
-                      Log.d("위치정보", "위도 : ${location.latitude} 경도 : ${location.longitude}")
-                        setLastLocation(location)
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult.let{
+                    for(location in it.locations) {
+                        Log.d("위치정보", "위도 : ${location.latitude}, 경도 : ${location.longitude}")
+                        if(flag == 1)
+                        {
+                            flag = 0
+                            moveMap(location)
+                        }
+                        movemarker(location)
                     }
                 }
             }
         }
 
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper())
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
     }
 
-    fun setLastLocation(lastLocation : Location){
-        val LATLNG = LatLng(lastLocation.latitude, lastLocation.longitude)
+    fun moveMap(location:Location){
 
-        val makerOptions = MarkerOptions().position(LATLNG).title("i am here")
-        val cameraPosition = CameraPosition.Builder().target(LATLNG).zoom(15.0f).build()
+        Log.d("위치정보", "-----moveMap 표시시작!")
+        val latLng = LatLng(location.latitude, location.longitude)
+        val position = CameraPosition.Builder()
+            .target(latLng)
+            .zoom(16f)
+            .build()
 
-        mMap?.clear()
-        mMap?.addMarker(makerOptions)
-        mMap?.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        map.moveCamera((CameraUpdateFactory.newCameraPosition(position)))
+
+
+        Log.d("위치정보", "-----moveMap 표시끝!")
     }
+
+    fun movemarker(location: Location)
+    {
+        Log.d("위치정보", "마커정보!")
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("현위치")
+
+        map.addMarker(markerOptions)
+    }
+
+
 }
